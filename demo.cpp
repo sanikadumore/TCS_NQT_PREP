e2caaf8e33e299e0dbee668067a7a16af59333f3
sanikadumore <sanikadumore@gmail.com>
2026-06-20 23:55:57 +0530

    Hello.java

---
+++ demo.cpp
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    int a;//123
+    scanf("%d",&a);
+    printf("value of %d",a);//value of 123
+}
---
+++ demo2.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    //cin>>a;//run timr user input
+    cout<<a;//address of the variable , print statement
+}
